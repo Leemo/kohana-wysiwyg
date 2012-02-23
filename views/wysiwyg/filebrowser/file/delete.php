391d36d86f1167964dd72f88b808305d7f28c74c
Alexeyco <6425762@gmail.com>
2012-02-23 21:21:39 +0400

    Removed unused view files

--- views/wysiwyg/filebrowser/file/delete.php
+++
@@ -1,8 +0,0 @@
-<h1><?php echo __('Delete file') ?></h1>
-<p><?php echo __('You are trying to delete a file :filename.', array(':filename' => '<strong>'.$filename.'</strong>')) ?></p>
-<p><?php echo __('Would be impossible to restore it, and all external links on this file, if any, are unavailable.') ?></p>
-<p><?php echo __('Are you sure?') ?></p>
-<div class="buttons">
-	<?php echo HTML::anchor(Request::current()->uri().URL::query(array('agree' => 1)), __('Yes, delete it'), array('class' => 'button gray medium ajaxed')) ?>
-	<?php echo HTML::anchor('#', __('No, I changed my mind'), array('class' => 'button gray medium close')) ?>
-</div>
