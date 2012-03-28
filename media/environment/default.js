223bbcaad8c91a654b4013a7c25cb7e3d7b27597
Afanasyev-Alexander <alex@neonlux.ru>
2012-03-28 17:18:20 +0400

    replace code of ckeditor html-writer rules setting to environment/default.js

--- media/environment/default.js
+++ media/environment/default.js
@@ -1,21 +1,42 @@
 (function($) {
-  $(document).ready(function() {
-    var wysiwyg_config = {
+  var wysiwyg_config = {
 
-      // Main configuration
-      skin:        "kama",
-      filebrowser: true,
+    // Main configuration
+    skin:        "kama",
+    filebrowser: true,
 
-      // Filebrowser settings
-      filebrowserBrowseUrl:      "wysiwyg/filebrowser/browse",
-      filebrowserImageBrowseUrl: "wysiwyg/filebrowser/images",
-      filebrowserFlashBrowseUrl: "wysiwyg/filebrowser/flash",
-      filebrowserUploadUrl:      "wysiwyg/filebrowser/upload",
-      filebrowserImageUploadUrl: "wysiwyg/filebrowser/upload",
-      filebrowserFlashUploadUrl: "wysiwyg/filebrowser/upload"
-    };
+    // Filebrowser settings
+    filebrowserBrowseUrl:      "wysiwyg/filebrowser/browse",
+    filebrowserImageBrowseUrl: "wysiwyg/filebrowser/images",
+    filebrowserFlashBrowseUrl: "wysiwyg/filebrowser/flash",
+    filebrowserUploadUrl:      "wysiwyg/filebrowser/upload",
+    filebrowserImageUploadUrl: "wysiwyg/filebrowser/upload",
+    filebrowserFlashUploadUrl: "wysiwyg/filebrowser/upload"
+  };
+
+  $(function() {
 
     // Понеслася! (rus. "Here we go!") :)
     $(".rte").ckeditor(wysiwyg_config);
+
+    // set output HTML code rules
+    CKEDITOR.on( 'instanceReady', function(ev) {
+      var writer = ev.editor.dataProcessor.writer,
+
+      tags = ['p','h1','h2','h3','h4','h5','h6','dl','dt','dd','div','ul','li'];
+
+      writer.indentationChars = '   ';
+
+      for (var i=0; i<tags.length; i++) {
+        writer.setRules(tags[i],
+        {
+          indent : true,
+          breakBeforeOpen : true,
+          breakAfterOpen : true,
+          breakBeforeClose : true,
+          breakAfterClose : true
+        });
+      }
+    });
   })
 }(jQuery));
