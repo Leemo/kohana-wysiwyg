99bfa20e56d3a9ec8ec11cfd359afed061e8651f
Afanasyev-Alexander <alex@neonlux.ru>
2012-12-29 00:41:45 +0300

    Editor core forcedly updated to full version (to provide some required features). Toolbars restored. Non-actual tabs removed from link, flash and image dialogs, showblocks plugin added

--- media/ckeditor/config.js
+++ media/ckeditor/config.js
@@ -1,26 +1,23 @@
-/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
 CKEDITOR.editorConfig = function(config)
 {
 	config.toolbarGroups = [
-		{name: 'clipboard',   groups: [ 'clipboard', 'undo' ]},
-		{name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ]},
+		{name: 'clipboard',   groups: [ 'clipboard', 'undo', 'mode' ]},
 		{name: 'links'},
 		{name: 'insert'},
-		{name: 'forms'},
 		{name: 'tools'},
-		{name: 'document',	   groups: [ 'mode', 'document', 'doctools' ]},
 		'/',
-		{name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ]},
+		{name: 'basicstyles', groups: [ 'basicstyles', 'colors', 'cleanup' ]},
 		{name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ]},
-		{name: 'styles'},
-		{name: 'colors'},
-		{name: 'others', groups: ['Youtube', 'teaser']}
+		{name: 'styles'}
 	];
 
     config.extraPlugins = 'youtube,teaser,autogrow';
+    config.removeButtons = 'Smiley,CreateDiv,PageBreak,Iframe,Font,Blockquote';
+    config.removeDialogTabs = 'flash:Upload;image:Upload;link:upload;link:Advanced'
 
 };
