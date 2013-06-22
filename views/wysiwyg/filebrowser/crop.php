44358339661290afc641a34ddf7f09523a268efa
Alexeyco <6425762@gmail.com>
2013-06-22 23:22:01 +0400

    Static files includes moved to separate files

--- views/wysiwyg/filebrowser/crop.php
+++ views/wysiwyg/filebrowser/crop.php
@@ -6,34 +6,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9" />
 		<title><?php echo __('Crop and resize image :path', array(':path' => $path)) ?></title>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-		<?php $route = Route::get('media/wysiwyg') ?>
-<?php
-
-	$js = array
-	(
-		'filebrowser/jquery-1.7.1.js',
-		'filebrowser/jquery.form.js',
-		'filebrowser/cropresizer/jquery.cropresizer.js',
-		'filebrowser/bootstrap/bootstrap-modal.js',
-		'filebrowser/bootstrap/bootstrap-dropdown.js',
-		'filebrowser/cropresizer/global.js'
-	);
-
-	$css = array
-	(
-		'filebrowser/bootstrap/bootstrap.css',
-		'filebrowser/cropresizer/global.css'
-	);
-?>
-<?php foreach($js as $file): ?>
-		<?php echo HTML::script($route->uri(array('file' => $file))) ?>
-
-<?php endforeach ?>
-<?php foreach($css as $file): ?>
-		<?php echo HTML::style($route->uri(array('file' => $file))) ?>
-
-<?php endforeach ?>
-		<script type="text/javascript">$(function(){$("#img").cropResize({width:<?php echo $width ?>, height:<?php echo $height ?>})});</script>
+    <?php echo View::factory('wysiwyg/filebrowser/crop/static')->bind('width', $width)->bind('height', $height) ?>
 	</head>
 	<body>
 		<!-- Modal windows -->
